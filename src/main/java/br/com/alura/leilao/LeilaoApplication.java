35b88d99fca059618626632b80f03d54a7d61dfa
claramilanez <48100402+claramilanez@users.noreply.github.com>
2021-03-26 13:59:42 -0300

    Update LeilaoApplication.java

--- src/main/java/br/com/alura/leilao/LeilaoApplication.java
+++ src/main/java/br/com/alura/leilao/LeilaoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeilaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LeilaoApplication.class, arg); //trocando args por arg para provocar erro e enviar msg de email notificando pelo jenkins
+		SpringApplication.run(LeilaoApplication.class, args1); //trocando args por arg para provocar erro e enviar msg de email notificando pelo jenkins
 		//comentario qq para fazer commit e provocar o build no jenkins
 	}
 
